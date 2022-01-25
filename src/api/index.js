e000622f4633c3ef3db72890cc23361dc71d5c71
ZnoGouDj <znogoud3@gmail.com>
2022-01-25 19:13:01 +0300

    Add the weather icons

--- src/api/index.js
+++ src/api/index.js
@@ -22,3 +22,22 @@ export const getPlacesData = async (type, sw, ne) => {
     console.log(error);
   }
 };
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+      params: {
+        lon: lng,
+        lat: lat,
+      },
+      headers: {
+        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        'x-rapidapi-key': '759a24c1c6mshc1ecfe00fa9f9c3p107251jsnee515c7609b8',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
