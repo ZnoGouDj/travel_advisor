49e75dd276bd6af3a11aeae446c324e5628eb45f
ZnoGouDj <znogoud3@gmail.com>
2022-01-25 18:32:20 +0300

    Add scrolling to the clicked place

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import Map from './components/Map/Map';
 
 const App = () => {
   const [places, setPlaces] = useState([]);
+  const [childClicked, setChildClicked] = useState(null);
 
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState({});
+
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
@@ -19,8 +22,11 @@ const App = () => {
   }, []);
 
   useEffect(() => {
+    setIsLoading(true);
+
     getPlacesData(bounds.sw, bounds.ne).then(data => {
       setPlaces(data?.filter(place => place.name));
+      setIsLoading(false);
     });
   }, [coordinates, bounds]);
 
@@ -30,10 +36,16 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List places={places} />
+          <List places={places} childClicked={childClicked} isLoading={isLoading} />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} places={places} />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={places}
+            setChildClicked={setChildClicked}
+          />
         </Grid>
       </Grid>
     </>
